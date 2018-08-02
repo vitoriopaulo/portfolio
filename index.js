adc1b3b60324aba193e35a8c53a292ecd9770998
vitoriopaulo <vitoriopaulo@gmail.com>
2018-08-02 13:33:21 -0400

    Improvement in the UX|UI Design, Visual Design, Information architecture and usability were done. A Go back to top button was added. Such feature is welcomed in sites that is designed with the content distributed vertically such as this one.

--- index.js
+++ index.js
@@ -11,7 +11,22 @@ $(() => {
   	        window.location.hash = target
   	    })
   	})
+
+    $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $('#scroll').fadeIn();
+            } else {
+                $('#scroll').fadeOut();
+            }
+        });
+        $('#scroll').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+
 })
+
+
 
 // Imported from my codepen.io Account //
 
